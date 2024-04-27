3592d2cac0b4ec33c347875b66926fab487f750f
Sweta Shaw <swt.shw94@gmail.com>
2024-04-27 15:25:28 +0530

    add gas sponsor invoker contract example

---
+++ test/GasSponsorInvoker.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {Test} from "forge-std/Test.sol";
+import "forge-std/console.sol";
+import {VmSafe} from "forge-std/Vm.sol";
+
+import {GasSponsorInvoker} from "../src/GasSponsorInvoker.sol";
+import {SenderRecorder} from "../src/SenderRecorder.sol";
+
+contract GasSponsorInvokerTest is Test {
+    GasSponsorInvoker public gasSponsorInvoker;
+    SenderRecorder public senderRecorder;
+
+    VmSafe.Wallet public authority;
+
+    function setUp() public {
+        authority = vm.createWallet("authority");
+        gasSponsorInvoker = new GasSponsorInvoker();
+        senderRecorder = new SenderRecorder();
+    }
+
+    function test_invoke() external {
+
+        address initial_sender = senderRecorder.lastSender();
+
+        assertEq(initial_sender, address(0));
+
+        uint256 nonce = vm.getNonce(authority.addr);
+
+        bytes32 commitEncoded = bytes32(abi.encode("commit"));
+
+        bytes32 digest = gasSponsorInvoker.getDigest(commitEncoded, nonce);
+        (uint8 _v, bytes32 _r, bytes32 _s) = vm.sign(authority.privateKey, digest);
+
+        bytes memory data = abi.encodeWithSelector(
+            senderRecorder.recordSender.selector
+        );
+
+        gasSponsorInvoker.sponsorCall(
+            authority.addr,
+            commitEncoded,
+            _v,
+            _r,
+            _s,
+            address(senderRecorder),
+            data
+        );
+        assertEq(senderRecorder.lastSender(), authority.addr);
+    }
+}
